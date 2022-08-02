9177c92def5338e16ae8ed837013b75b8f98f451
Sarobidy-23 <100074450+Sarobidy-23@users.noreply.github.com>
2022-08-02 18:56:23 +0300

    Update Card.tsx

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,78 +1,77 @@
 import React, {useState, useEffect} from 'react'
 import { Markup } from 'interweave'
-import fake from '../data.json'
-import './Card.css'
-import { Base64 } from 'aws-sdk/clients/ecr'
+import './Result.css'
 
-export default function Card(props: any) {
-  const [key, setKey] = useState<any>()
-  const [value, setValue] = useState<any[]>([])
-  let datas = props
+export default function Card(datas: any) {
+  const [key, setKey] = useState<String[]>([])
+  const [value, setValue] = useState<String[]>([])
+
+  const mapData = (data: any[])=>{
+    let tableKey = []
+    let tableValue = []
+    
+    //Index is all of principal key (AgeRange, BoundigBox, ...)
+    let index = Object.keys(data[0])
+    for(let item of index){
+      tableKey.push(item)
+        if(typeof(data[0][item]) == 'number'){
+          tableValue.push((data[0][item]));
+        }else if(data[0][item].length){
+           tableValue.push(lenght(data[0][item]))
+        }else{
+          tableValue.push(parse((data[0][item])));
+        }
+    }
+    //Assign the key values at variable state ​​to be able to use them in the JSX render
+    setKey(tableKey);
+    setValue(tableValue)
+  }
 
   const parse = (table: any) => {
-    let cle = Object.keys(table); 
+    let clefs = Object.keys(table); 
     let out = ""
-    for(let one of cle){
-      out+=`\n${one} : ${table[one]}`
+    for(let clef of clefs){
+      out+=`\n${clef} : ${table[clef]}`
     }
   return out
   }
   
-const lenght = (tableLength: any) => {
-  let cle = Object.keys(tableLength[0]);
-  let out = "<table><tbody><tr>"
-    for(let one of cle){
-      out+=`<td class='titre'> ${one}</td>`
-    }
-    out += "</tr>"
-    for(let i=0; i<tableLength.length; i++){
-      out += "<tr class='tab'>"
-      for(let one of cle){
-        out += `<td>${tableLength[i][one]}</td>`
+  const lenght = (tableLength: any[]) => {
+    let clefs = Object.keys(tableLength[0]);
+    let out = "<table><tbody><tr>"
+      for(let clef of clefs){
+        out+=`<td class='titre'> ${clef}</td>`
       }
       out += "</tr>"
-    } 
+      for(let i=0; i<tableLength.length; i++){
+        out += "<tr class='tab'>"
+        for(let clef of clefs){
+          out += `<td>${tableLength[i][clef]}</td>`
+        }
+        out += "</tr>"
+      } 
+      
+      return out+="</tbody></table>"
+    }
     
-    return out+="</tbody></table>"
-}
-
-const mapData = (data: any)=>{
-  let tableKey = []
-  let tableValue = []
-  let index = Object.keys(data[0])
-  for(let item of index){
-    tableKey.push(item)
-      if(typeof(data[0][item]) == 'number'){
-        tableValue.push((data[0][item]));
-      }else if(data[0][item].length){
-         tableValue.push(lenght(data[0][item]))
-      }else{
-        tableValue.push(parse((data[0][item])));
-      }
-  }
-  setKey(tableKey);
-  setValue(tableValue)
-}
-
-useEffect(()=>{
-  if(datas){
-    // mapData(datas.datas);
-    mapData(fake);
-  }
-}, [datas])
+  useEffect(()=>{
+    if(datas){
+      mapData(datas.datas);
+    }
+  }, [datas])
 
   return (
-    <>
-      <table className='container'>
-        <tbody>
-          {value.map((elt: any, idx: number)=>(
-            <tr key={idx} className='bar'>
-              <th className='bar'>{key[idx]}</th>
-              <td className='content '><Markup content={(elt).toString()}/></td>
-            </tr>
-          ))}
-        </tbody>
+      <>
+        <table className='container'>
+          <tbody>
+            {value.map((elt: String, index: number)=>(
+              <tr key={index} className='bar'>
+                <th className='bar'>{key[index]}</th>
+                <td className='content '><Markup content={(elt).toString()}/></td>
+              </tr>
+            ))}
+          </tbody>
         </table>        
-    </>
-  )
+      </>
+    )
 }
